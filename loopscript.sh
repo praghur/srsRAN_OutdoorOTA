dae08a657e1ce5faf12cb185e981c7c1308ba5e7
praghur <166060957+praghur@users.noreply.github.com>
2025-01-16 16:49:32 -0500

    Update loopscript.sh

--- loopscript.sh
+++ loopscript.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Function to run traceroute 40 times randomly within an hour
+# Function to run traceroute 40 times randomly within an hour and write results to a CSV file
 run_traceroute() {
   for i in {1..40}
   do
+    result=$(traceroute -U -f 2 -m 2 -p 33435 10.45.2.10)
+    echo "$(date), $result" >> traceroute_results.csv
     traceroute -U -f 2 -m 2 -p 33435 10.45.2.10
     sleep $((RANDOM % 90 + 30))  # Sleep for a random time between 30 and 120 seconds
   done
