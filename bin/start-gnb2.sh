255704805248e7469f0affbd81600cb49c7ce8ba
praghur <166060957+praghur@users.noreply.github.com>
2025-01-16 10:14:54 -0500

    Update start-gnb2.sh

--- bin/start-gnb2.sh
+++ bin/start-gnb2.sh
@@ -3,4 +3,4 @@ tmux new-session -d -s gnb2
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t gnb2
-sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb  -c /local/repository/etc/srsran/gnb_x310_2.yml
+sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb  -c /local/repository/etc/srsran/gnb_x310_2.yml -c /local/repository/etc/srsran/qos.yml
